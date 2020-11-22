01a7f76220e293bdea1f5a80926b5a193db57a24
NeupaneKhemRaj <shekharneupane@gmail.com>
2020-11-22 19:56:47 +0200

    netlify A2

--- src/constants/CONSTANTS.ts
+++ src/constants/CONSTANTS.ts
@@ -1,6 +1,7 @@
-export const API_URL_USERS = 'http://localhost:3001/api/v1/users' //'https://khem-library.herokuapp.com/api/v1/users'
-export const API_URL_BOOKS = 'http://localhost:3001/api/v1/books' //'https://khem-library.herokuapp.com/api/v1/books'
-export const API_URL_LOGIN = 'http://localhost:3001/api/v1/login' //'https://khem-library.herokuapp.com/api/v1/login'
+export const API_URL_USERS = 'https://khem-library.herokuapp.com/api/v1/users'
+export const API_URL_BOOKS = 'https://khem-library.herokuapp.com/api/v1/books'
+export const API_URL_LOGIN = 'https://khem-library.herokuapp.com/api/v1/login'
 export const API_URL_GOOGLE_LOGIN = 'http://localhost:3001/api/v1/auth/google/'
-export const API_URL_AUTHORS = 'http://localhost:3001/api/v1/authors' //'https://khem-library.herokuapp.com/api/v1/authors'
-export const API_URL_LOANS = 'http://localhost:3001/api/v1/loans' //'https://khem-library.herokuapp.com/api/v1/loans'
+export const API_URL_AUTHORS =
+  'https://khem-library.herokuapp.com/api/v1/authors'
+export const API_URL_LOANS = 'https://khem-library.herokuapp.com/api/v1/loans'
