eeb4557240413023fae6a9075dc6ad585a6a000c
Khem Raj Neupane <khemrajneupane@Khems-MacBook-Pro-2.local>
2022-10-09 20:40:36 +0300

    added new api endpoint routes

--- Dockerfile
+++ Dockerfile
@@ -1,19 +1,19 @@
 FROM node:alpine
 
 # Set the working directory to /api
-#WORKDIR /app
+WORKDIR /app
 
 # copy package.json into the container at /api
-#COPY package*.json yarn.lock /app/
+COPY package*.json /app/
 
 # install dependencies
-RUN yarn install
+RUN npm install
 
 # Copy the current directory contents into the container at /api
 COPY . /app/
 
 # Make port 80 available to the world outside this container
-#EXPOSE 3000
+
 
 # Run the app when the container launches
-CMD ["yarn", "run", "start"]
+CMD ["npm", "run", "start"]
--- src/constants/CONSTANTS.ts
+++ src/constants/CONSTANTS.ts
@@ -1,10 +1,10 @@
-export const API_URL_USERS = 'https://khem-library.herokuapp.com/api/v1/users' //'http://localhost:3001/api/v1/users'
+export const API_URL_USERS = 'https://immense-thicket-33432.herokuapp.com/api/v1/users' //'http://localhost:3001/api/v1/users'
 export const API_URL_USERS_PP_CHANGE =
-  'https://khem-library.herokuapp.com/api/v1/users/changePP' //'http://localhost:3001/api/v1/users/changePP'
-export const API_URL_BOOKS = 'https://khem-library.herokuapp.com/api/v1/books' //'http://localhost:3001/api/v1/books'
-export const API_URL_LOGIN = 'https://khem-library.herokuapp.com/api/v1/login' //'http://localhost:3001/api/v1/login'
+  'https://immense-thicket-33432.herokuapp.com/api/v1/users/changePP' //'http://localhost:3001/api/v1/users/changePP'
+export const API_URL_BOOKS = 'https://immense-thicket-33432.herokuapp.com/api/v1/books' //'http://localhost:3001/api/v1/books'
+export const API_URL_LOGIN = 'https://immense-thicket-33432.herokuapp.com/api/v1/login' //'http://localhost:3001/api/v1/login'
 export const API_URL_GOOGLE_LOGIN =
-  'https://khem-library.herokuapp.com/api/v1/auth/google'
+  'https://immense-thicket-33432.herokuapp.com/api/v1/auth/google'
 export const API_URL_AUTHORS =
-  'https://khem-library.herokuapp.com/api/v1/authors' //'http://localhost:3001/api/v1/authors'
-export const API_URL_LOANS = 'https://khem-library.herokuapp.com/api/v1/loans' //'http://localhost:3001/api/v1/loans'
+  'https://immense-thicket-33432.herokuapp.com/api/v1/authors' //'http://localhost:3001/api/v1/authors'
+export const API_URL_LOANS = 'https://immense-thicket-33432.herokuapp.com/api/v1/loans' //'http://localhost:3001/api/v1/loans'
