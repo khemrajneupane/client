9e6def3aae446bcb63a2e8336d3a0233d5becfe1
Khem Raj Neupane <khemrajneupane@Khems-MacBook-Pro-2.local>
2024-06-16 20:14:50 +0300

    deploy v4

--- src/constants/CONSTANTS.ts
+++ src/constants/CONSTANTS.ts
@@ -1,10 +1,10 @@
-export const API_URL_USERS = 'https://immense-thicket-33432.herokuapp.com/api/v1/users' //'http://localhost:3001/api/v1/users'
+export const API_URL_USERS = 'https://khem-library.onrender.com/api/v1/users' //'http://localhost:3001/api/v1/users'
 export const API_URL_USERS_PP_CHANGE =
-  'https://immense-thicket-33432.herokuapp.com/api/v1/users/changePP' //'http://localhost:3001/api/v1/users/changePP'
-export const API_URL_BOOKS = 'https://immense-thicket-33432.herokuapp.com/api/v1/books' //'http://localhost:3001/api/v1/books'
-export const API_URL_LOGIN = 'https://immense-thicket-33432.herokuapp.com/api/v1/login' //'http://localhost:3001/api/v1/login'
+  'https://khem-library.onrender.com/api/v1/users/changePP' //'http://localhost:3001/api/v1/users/changePP'
+export const API_URL_BOOKS = 'https://khem-library.onrender.com/api/v1/books' //'http://localhost:3001/api/v1/books'
+export const API_URL_LOGIN = 'https://khem-library.onrender.com/api/v1/login' //'http://localhost:3001/api/v1/login'
 export const API_URL_GOOGLE_LOGIN =
-  'https://immense-thicket-33432.herokuapp.com/api/v1/auth/google'
+  'https://khem-library.onrender.com/api/v1/auth/google'
 export const API_URL_AUTHORS =
-  'https://immense-thicket-33432.herokuapp.com/api/v1/authors' //'http://localhost:3001/api/v1/authors'
-export const API_URL_LOANS = 'https://immense-thicket-33432.herokuapp.com/api/v1/loans' //'http://localhost:3001/api/v1/loans'
+  'https://khem-library.onrender.com/api/v1/authors' //'http://localhost:3001/api/v1/authors'
+export const API_URL_LOANS = 'https://khem-library.onrender.com/api/v1/loans' //'http://localhost:3001/api/v1/loans'
